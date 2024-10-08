e4184a8037496fcd3010997b6c93e3bb62dc6d54
iiryan <iirharbi@gmail.com>
2024-10-08 18:07:44 +0300

    initial commit

---
+++ src/main/java/com/rayan/salarytracker/SalarytrackeraapiApplication.java
@@ -0,0 +1,13 @@
+package com.rayan.salarytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalarytrackeraapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalarytrackeraapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rayan/salarytracker/SalarytrackeraapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayan.salarytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalarytrackeraapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
