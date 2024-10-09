a46754185e9abd66d6fa93b60979fdda723d9222
iiryan <iirharbi@gmail.com>
2024-10-09 19:21:08 +0300

    USe a proper status response for PUT & POST

--- src/main/java/com/rayan/salarytracker/controller/SalaryController.java
+++ src/main/java/com/rayan/salarytracker/controller/SalaryController.java
@@ -3,6 +3,7 @@ package com.rayan.salarytracker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rayan.salarytracker.entity.Salary;
@@ -33,12 +35,13 @@ public class SalaryController {
         return salaryService.getSalaryById(id);
     }
 
-    // TODO: Change return type to proper Response.
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteSalaryById(@PathVariable("id") Long id){
         salaryService.deleteSalary(id);
     }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping
     public Salary insertSalary(@RequestBody Salary theSalary){
         System.out.println("Inserting the salary... " + theSalary);
