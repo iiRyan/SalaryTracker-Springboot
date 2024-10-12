292ad732cc8969fb497b9d6dab6de7cf4c3a39f3
iiryan <iirharbi@gmail.com>
2024-10-12 23:20:27 +0300

    Add InMemoryUserDetailsManager

--- src/main/java/com/rayan/salarytracker/config/WebSecurityConfig.java
+++ src/main/java/com/rayan/salarytracker/config/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package com.rayan.salarytracker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
-
 
 @Configuration
 public class WebSecurityConfig {
@@ -13,13 +15,44 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())  // Disable CSRF protection
-        .authorizeHttpRequests(authz -> authz
-            .requestMatchers("/auth/login", "/auth/register").permitAll()  // Public endpoints
-            .anyRequest().authenticated()  // Secure all other endpoints
-        )
-        .httpBasic(Customizer.withDefaults());  // Basic authentication
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/auth/login", "/auth/register").permitAll() // Public endpoints
+                        .anyRequest().authenticated() // Secure all other endpoints
+                )
+                .httpBasic(Customizer.withDefaults()); // Basic authentication
 
-    return http.build();
+        return http.build();
     }
+
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsService() {
+    //     UserDetails user = User.withUsername("Rayan")
+    //             .password(passwordEncoder().encode("123456"))
+    //             .roles("USER")
+    //             .build();
+    //     return new InMemoryUserDetailsManager(user);
+    // }
+
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("rayan")
+                .password("{noop}test123")
+                .roles("admin")
+                .build();
+        UserDetails mary = User.builder()
+                .username("jojo")
+                .password("{noop}test123")
+                .roles("user")
+                .build();
+       
+        return new InMemoryUserDetailsManager(john, mary);
+    }
+
 }
