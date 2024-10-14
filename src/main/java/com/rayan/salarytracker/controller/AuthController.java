d4f3f9d54d41d35a4c28bc147aa4b5cd75ff0285
iiryan <iirharbi@gmail.com>
2024-10-14 18:36:28 +0300

    Add support fro JWT Token

--- src/main/java/com/rayan/salarytracker/controller/AuthController.java
+++ src/main/java/com/rayan/salarytracker/controller/AuthController.java
@@ -4,18 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rayan.salarytracker.entity.AuthModel;
+import com.rayan.salarytracker.entity.JwtResponse;
 import com.rayan.salarytracker.entity.User;
 import com.rayan.salarytracker.entity.UserModel;
+import com.rayan.salarytracker.security.CustomUserDetailsService;
 import com.rayan.salarytracker.service.UserService;
+import com.rayan.salarytracker.util.JwtTokenUtil;
 
 import jakarta.validation.Valid;
 
@@ -27,16 +33,33 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserService userService;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody AuthModel authModel) {
-      Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(), authModel.getPassword()));
-      SecurityContextHolder.getContext().setAuthentication(authentication); // here we going to save the logged in user.
-      return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    public ResponseEntity<JwtResponse> login(@RequestBody AuthModel authModel) throws Exception {
+        authenticate(authModel.getEmail(), authModel.getPassword());
+        // Generate jtw token
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authModel.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        
+        return new ResponseEntity<JwtResponse>(new JwtResponse(token), HttpStatus.OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<User> saveUser(@Valid @RequestBody UserModel theUser) {
         return new ResponseEntity<User>(userService.createUser(theUser), HttpStatus.CREATED);
+    }
+
+    private void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (DisabledException e) {
+            throw new Exception("User disable");
+        } catch (BadCredentialsException e) {
+            throw new Exception("Bad Credentials");
+        }
     }
 }
---
+++ src/main/java/com/rayan/salarytracker/entity/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.rayan.salarytracker.entity;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+	
+	private final String jwtToken;
+	
+	
+}
